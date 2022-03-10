51754b3d5b30ca4d10f059b64923e23e519af859
Haidien <thd4777@gmail.com>
2022-03-10 21:08:27 +0700

    first commit

---
+++ HaiDien_ThaiTuan_ThanhLuyen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HaiDien_ThaiTuan_ThanhLuyen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello my name is Dien");
+        }
+    }
+}
